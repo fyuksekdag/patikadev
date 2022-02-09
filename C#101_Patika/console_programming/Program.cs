836590f0950a035b422eafcb3edce4ec406d1973
FıratYu <firatsyuksekdag@gmail.com>
2022-02-09 20:36:20 +0300

    C#101

---
+++ C#101_Patika/console_programming/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Adınız:");
+string name = Console.ReadLine();
+Console.WriteLine("Soyadınız:");
+string surname = Console.ReadLine();
+
+Console.WriteLine("Merhaba "+ name + " " + surname);
