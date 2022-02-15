9d25f47a99c9670976cc6e0c823837506703eec5
FıratYu <firatsyuksekdag@gmail.com>
2022-02-15 22:57:07 +0300

    1

---
+++ C#101_Patika/Donguler/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Lütfen bir sayı giriniz: ");
+int girdi = int.Parse(Console.ReadLine());
+
+for (int i = 0; i <= girdi; i++)
+{
+    if(i%2 != 0)
+        Console.WriteLine(i);
+}
+int toplam = 0;
+for (int i = 0; i <= 100; i++)
+{
+    if(i%2 == 0)
+        toplam += i;
+}
+Console.WriteLine(toplam);
