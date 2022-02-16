d0461580613a474f27359bbb267ca2e355d8a979
FıratYu <firatsyuksekdag@gmail.com>
2022-02-16 12:29:46 +0300

    1

---
+++ C#101_Patika/While_Foreach/Program.cs
@@ -0,0 +1,29 @@
+﻿int toplam = 0;
+int n = int.Parse(Console.ReadLine());
+int i = 0;
+while (i <= n)
+{
+    toplam += i;
+    i++;
+}
+float ort = (toplam / n);
+Console.WriteLine("Girdiğiniz sayıların ortalaması: " + ort);
+
+// a'dan z'ye
+char c = 'a';
+while (c <= 'z')
+{
+    Console.Write(c);
+    c++;
+}
+
+Console.WriteLine("******* Foreach ********");
+
+string[] arabalar = {"Mercedes", "BMW", "Audi", "Opel"};
+
+
+foreach (var araba in arabalar)  // var tipi belli olmayan, genel bir tanımlama, generic
+{
+    int uzunluk = araba.Length;
+    Console.WriteLine(araba + " Harf sayısı: "+ uzunluk);
+}
