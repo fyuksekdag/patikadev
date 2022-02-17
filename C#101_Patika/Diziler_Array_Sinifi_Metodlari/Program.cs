16551faa811e34ef5665460d61e9485f4d07fe8d
FıratYu <firatsyuksekdag@gmail.com>
2022-02-17 17:43:24 +0300

    1

---
+++ C#101_Patika/Diziler_Array_Sinifi_Metodlari/Program.cs
@@ -0,0 +1,43 @@
+﻿//Sort
+
+int[] sayiDizisi = {45,22,563,363,1231,353,6};
+Console.WriteLine("Sırasız Dizi");
+foreach (var item in sayiDizisi)
+{
+    Console.WriteLine(item);
+}
+Console.WriteLine("Sıralı Dizi");
+Array.Sort(sayiDizisi); //bu bir void değişken, verdiğim dizi üzerinde değişiklik yapar. Yeni bir atama yapmama gerek yok.
+foreach (var item in sayiDizisi)
+{
+    Console.WriteLine(item);
+}
+//Clear (Dizi içerisinde verdiğimiz indexten başlayarak verdiğimiz eleman sayısı kadar 0 atıyor.)
+Console.WriteLine("Array Clear");
+
+Array.Clear(sayiDizisi,2,2);
+foreach (var item in sayiDizisi)
+{
+    Console.WriteLine(item);
+}
+//Reverse (Diziyi tersten yazdırır, sıralamaz.)
+Console.WriteLine("Array Reverse");
+Array.Reverse(sayiDizisi);
+foreach (var item in sayiDizisi)
+{
+    Console.WriteLine(item);
+}
+//IndexOf (Kacıncı sırada)
+Console.WriteLine("Array IndexOf");
+
+Console.WriteLine(Array.IndexOf(sayiDizisi, 563));
+
+//Resize
+Console.WriteLine("Array Resize");
+Array.Resize<int>(ref sayiDizisi, 9);
+sayiDizisi[7] = 55;
+sayiDizisi[8] = 931;
+foreach (var item in sayiDizisi)
+{
+    Console.WriteLine(item);
+}
