67cf7e477ac2a2ed78dfdc3607250b6cd04ef5da
FıratYu <firatsyuksekdag@gmail.com>
2022-04-09 15:24:09 +0300

    patika

---
+++ C#101_Patika/Metotlar_2_Overloading/Program.cs
@@ -0,0 +1,47 @@
+﻿// out parametreler
+string sayi = "999";
+int outSayi;
+
+bool sonuc = int.TryParse(sayi, out outSayi);
+if (sonuc)
+{
+    Console.WriteLine("Dönüşüm gerçekleştirildi.");
+}
+else
+{
+    Console.WriteLine("Dönüşüm gerçekleştirilemedi.");
+}
+Metotlar instance = new Metotlar();
+instance.Topla(4,2, out int toplamSonuc); // return yerine out kullanılabilir
+Console.WriteLine(toplamSonuc);
+
+// Metot Overloading
+
+int number = 987;
+instance.EkranaYazdir(Convert.ToString(number));
+instance.EkranaYazdir(number);
+
+instance.EkranaYazdir("Python is", " the best");
+
+//Metot Imzası
+//metotAdı + parametre sayisi + parametre (Overloading olabilmesi için sadece bunlar farklılaştırılabilir.)
+
+class Metotlar
+{
+    public void Topla(int a, int b, out int toplam)
+    {
+        toplam = a+b;
+    }
+    public void EkranaYazdir(string veri)
+    {
+        Console.WriteLine(veri);
+    }
+    public void EkranaYazdir(int veri)
+    {
+        Console.WriteLine(veri);
+    }
+    public void EkranaYazdir(string veri, string veri2)
+    {
+        Console.WriteLine(veri + veri2);
+    }
+}
