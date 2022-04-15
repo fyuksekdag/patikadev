c0d5ef143fc35452bd821a0924d64649e9fc1efe
FıratYu <firatsyuksekdag@gmail.com>
2022-04-15 17:02:00 +0300

    patika

---
+++ C#101_Patika/Koleksiyonlar_4_Dictionary/Program.cs
@@ -0,0 +1,38 @@
+﻿Dictionary<int, string> kullanicilar = new Dictionary<int, string>();
+
+kullanicilar.Add(10, "Can Ağar");
+kullanicilar.Add(12, "Banu Elmacı");
+kullanicilar.Add(15, "Ece Kara");
+kullanicilar.Add(22, "Özcan Çoşar");
+
+//Dizi elemanlarına erişim
+Console.WriteLine(kullanicilar[12]);
+foreach (var item in kullanicilar)
+{
+    Console.WriteLine(item);
+}
+
+//Count
+Console.WriteLine(kullanicilar.Count);
+
+//Contains
+Console.WriteLine(kullanicilar.ContainsKey(12));
+Console.WriteLine(kullanicilar.ContainsValue("Banu Elmacı"));
+
+//Remove
+kullanicilar.Remove(22);
+foreach (var item in kullanicilar)
+{
+    Console.WriteLine(item.Value);
+}
+
+//Keys
+foreach (var item in kullanicilar.Keys)
+{
+    Console.WriteLine(item);
+}
+
+//Values
+foreach (var item in kullanicilar.Values){
+    Console.WriteLine(item);
+}
