27a7121d2e9aef84667b0ba2ce4a45ae53426456
FıratYu <firatsyuksekdag@gmail.com>
2022-04-16 15:31:16 +0300

    Patika

---
+++ C#101_Patika/Odevler/Odev_2/Koleksiyonlar-Soru-3/Program.cs
@@ -0,0 +1,21 @@
+﻿//Klavyeden girilen cümle içerisindeki sesli harfleri bir dizi içerisinde saklayan ve dizinin elemanlarını sıralayan programı yazınız.
+using System.Collections;
+
+Console.WriteLine("Lütfen bir cümle giriniz.");
+string sentence = Console.ReadLine();
+sentence = sentence.ToLower();
+
+string vowels = "aeıioöuü";
+
+ArrayList words = new ArrayList();
+for (int i = 0; i < sentence.Length; i++)
+{
+    if (vowels.Contains(sentence[i]))
+        words.Add(sentence[i]);
+}
+//Sıralama
+words.Sort();
+foreach (var item in words)
+{
+    Console.Write(item);
+}
