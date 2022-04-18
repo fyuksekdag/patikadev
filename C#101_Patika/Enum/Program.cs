7500b328d7737e14cc0b33d53704b2158f1360f2
FıratYu <firatsyuksekdag@gmail.com>
2022-04-18 15:07:54 +0300

    Enum

---
+++ C#101_Patika/Enum/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine(Gunler.Salı);
+Console.WriteLine((int)Gunler.Salı);
+Console.WriteLine(Gunler.Cumartesi);
+Console.WriteLine((int)Gunler.Cumartesi);
+
+
+
+int sicaklik = 20;
+
+if (sicaklik < (int)HavaDurumu.Normal)
+    Console.WriteLine("Hava soğuk");
+else if(sicaklik >= (int)HavaDurumu.Sicak)
+    Console.WriteLine("Hava Sıcak");
+else if(sicaklik >= (int)HavaDurumu.Normal && sicaklik <= (int)HavaDurumu.Sicak)
+    Console.WriteLine("Hava gayet iyi");
+enum Gunler{
+    Pazartesi = 1,
+    Salı,
+    Çarşamba,
+    Perşembe,
+    Cuma = 23,
+    Cumartesi,
+    Pazar
+}
+
+enum HavaDurumu
+{
+    Soguk = 0,
+
+    Normal = 15,
+    
+    Sicak = 25,
+
+    CokSicak = 35
+}
